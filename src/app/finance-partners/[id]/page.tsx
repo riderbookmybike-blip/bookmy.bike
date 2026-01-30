26c874237f4b322aea1a4bef43d149b1bce7f4a8
Rathore Ajit M Singh <rathoreajitmsingh@Rathores-MacBook-Air.local>
2026-01-30 05:43:59 +0530

    refactor: Remove old finance-partners directory
    - Moved finance partners to dashboard/finance-partners
    - Removed duplicate files from old location
    - Consolidated finance partner management in new structure

--- src/app/finance-partners/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import BankFullDetail from '../BankFullDetail';
-
-export default async function BankDetailPage({ params }: { params: Promise<{ id: string }> }) {
-    const { id } = await params;
-    return <BankFullDetail id={id} />;
-}
